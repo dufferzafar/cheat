a7e5411593808e70e2f78ce4be0f119dadf364a8
Shadab Zafar <dufferzafar0@gmail.com>
2014-07-02 17:55:49 +0530

    Pretty output + Numbered commands = Better Cheats
    The file is now read line by line, using 'bufio' package and some sort
    of a scanner. Reminds me of Java. Thanks StackOverflow!
    
    Currently the colored output is produced just by using the ANSI escape
    codes: http://en.wikipedia.org/wiki/ANSI_escape_code#Colors
    
    But later, something more feature blown can be used:
    https://github.com/wsxiaoys/terminal/
    
    Comments are printed in yellow.
    
    Commands are prefixed with a number, that will be used when doing stuff
    like:
    
        cheat git 12 		# Execute the 12th command
    
        cheat git --copy 12		# Copy 12th command to clipboard
    
    The errors are now printed to stderror, and exit with a status of 1.
    
    When no args are passed, help is shown.

--- cheat.go
+++ cheat.go
@@ -3,14 +3,16 @@
 package main
 
 import (
+    "bufio"
     "fmt"
     "github.com/codegangsta/cli"
-    "io/ioutil"
     "os"
     "path"
+    "strconv"
+    "strings"
 )
 
-const version string = "0.1"
+const version string = "0.2"
 
 // These settings will be stored in a json once we get to that point.
 const cheatdir string = "cheatsheets"
@@ -32,19 +34,36 @@ func main() {
 
 func mainCmd(c *cli.Context) {
     if !c.Args().Present() {
-        // Help will be displayed here
-        println("No Args were passed!")
+        cli.ShowAppHelp(c)
     } else {
         var cmdname = os.Args[1]
         var cheatfile = path.Join(cheatdir, cmdname)
 
         if _, err := os.Stat(cheatfile); os.IsNotExist(err) {
-            fmt.Printf("No cheatsheat found for '%s'\n", cmdname)
-            fmt.Printf("To create a new sheet, run: cheat -e %s\n", cmdname)
-            return
+            fmt.Fprintf(os.Stderr, "No cheatsheat found for '%s'\n", cmdname)
+            fmt.Fprintf(os.Stderr, "To create a new sheet, run: cheat -e %s\n", cmdname)
+            os.Exit(1)
         } else {
-            data, _ := ioutil.ReadFile(cheatfile)
-            fmt.Print(string(data))
+            file, _ := os.Open(cheatfile)
+            scanner := bufio.NewScanner(file)
+
+            var i = 1
+            for scanner.Scan() {
+                line := strings.Trim(scanner.Text(), " ")
+
+                // Pretty print the output
+                // Todo: Will have to be tested on other platforms and terminals.
+
+                if strings.HasPrefix(line, "#") {
+                    fmt.Println("\x1b[33;1m" + line + "\x1b[0m")
+                } else if strings.HasPrefix(line, cmdname) {
+                    fmt.Println("\x1b[36;1m(" + strconv.Itoa(i) + ")\x1b[0m " + line)
+                    i++
+                } else {
+                    fmt.Println(line)
+                }
+            }
+            file.Close()
         }
     }
 }
