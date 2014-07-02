f54f1d23d82769474f89c3fdbf0db791d6e3fb51
Shadab Zafar <dufferzafar0@gmail.com>
2014-07-02 16:24:05 +0530

    Gofmt now indents with 4 spaces instead of tabs
    wtf!

--- cheat.go
+++ cheat.go
@@ -3,11 +3,11 @@
 package main
 
 import (
-	"fmt"
-	"github.com/codegangsta/cli"
-	"io/ioutil"
-	"os"
-	"path"
+    "fmt"
+    "github.com/codegangsta/cli"
+    "io/ioutil"
+    "os"
+    "path"
 )
 
 const version string = "0.1"
@@ -16,35 +16,35 @@ const version string = "0.1"
 const cheatdir string = "cheatsheets"
 
 func main() {
-	app := cli.NewApp()
+    app := cli.NewApp()
 
-	// Use our custom templates
-	cli.AppHelpTemplate = AppHelpTemplate
+    // Use our custom templates
+    cli.AppHelpTemplate = AppHelpTemplate
 
-	app.Name = "cheat"
-	app.Usage = "Create and view command-line cheatsheets."
-	app.Version = version
+    app.Name = "cheat"
+    app.Usage = "Create and view command-line cheatsheets."
+    app.Version = version
 
-	app.Action = mainCmd
+    app.Action = mainCmd
 
-	app.Run(os.Args)
+    app.Run(os.Args)
 }
 
 func mainCmd(c *cli.Context) {
-	if !c.Args().Present() {
-		// Help will be displayed here
-		println("No Args were passed!")
-	} else {
-		var cmdname = os.Args[1]
-		var cheatfile = path.Join(cheatdir, cmdname)
+    if !c.Args().Present() {
+        // Help will be displayed here
+        println("No Args were passed!")
+    } else {
+        var cmdname = os.Args[1]
+        var cheatfile = path.Join(cheatdir, cmdname)
 
-		if _, err := os.Stat(cheatfile); os.IsNotExist(err) {
-			fmt.Printf("No cheatsheat found for '%s'\n", cmdname)
-			fmt.Printf("To create a new sheet, run: cheat -e %s\n", cmdname)
-			return
-		} else {
-			data, _ := ioutil.ReadFile(cheatfile)
-			fmt.Print(string(data))
-		}
-	}
+        if _, err := os.Stat(cheatfile); os.IsNotExist(err) {
+            fmt.Printf("No cheatsheat found for '%s'\n", cmdname)
+            fmt.Printf("To create a new sheet, run: cheat -e %s\n", cmdname)
+            return
+        } else {
+            data, _ := ioutil.ReadFile(cheatfile)
+            fmt.Print(string(data))
+        }
+    }
 }
