e816c09c025566f4a879c0e0b90341750da5a1e0
Shadab Zafar <dufferzafar0@gmail.com>
2015-05-11 19:07:19 +0530

    Rebirth, using sub-commands
    Needs a copy command

--- cheat.go
+++ cheat.go
@@ -3,15 +3,15 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "github.com/codegangsta/cli"
-    "io/ioutil"
-    "os"
-    "os/exec"
-    "path"
-    "strconv"
-    "strings"
+	"bufio"
+	"fmt"
+	"github.com/codegangsta/cli"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"strings"
 )
 
 const version string = "0.3"
@@ -20,82 +20,91 @@ const version string = "0.3"
 const cheatdir string = "cheatsheets"
 
 func main() {
-    app := cli.NewApp()
+	app := cli.NewApp()
 
-    // Use our custom templates
-    cli.AppHelpTemplate = AppHelpTemplate
+	// Use our custom templates
+	cli.AppHelpTemplate = AppHelpTemplate
 
-    app.Name = "cheat"
-    app.Usage = "Create and view command-line cheatsheets."
-    app.Version = version
+	app.Name = "cheat"
+	app.Usage = "Create and view command-line cheatsheets."
+	app.Version = version
 
-    app.Action = mainCmd
+	config := &JSONData{}
+	config.ReadConfig()
 
-    app.Flags = []cli.Flag{
-        cli.BoolFlag{"edit, e", "edit a cheatsheet"},
-    }
+	app.Commands = []cli.Command{
+		{
+			Name:  "show",
+			Usage: "Show cheats related to a command",
+			Action: func(c *cli.Context) {
+				var cmdname = c.Args().First()
+				var cheatfile = path.Join(cheatdir, cmdname)
 
-    app.Run(os.Args)
-}
+				if _, err := os.Stat(cheatfile); os.IsNotExist(err) {
+					fmt.Fprintf(os.Stderr, "No cheatsheat found for '%s'\n", cmdname)
+					fmt.Fprintf(os.Stderr, "To create a new sheet, run: cheat edit %s\n", cmdname)
+					os.Exit(1)
+				} else {
+					showCheats(cheatfile, cmdname)
+				}
+			},
+		},
+		{
+			Name:  "list",
+			Usage: "List all available cheats",
+			Action: func(c *cli.Context) {
+				files, _ := ioutil.ReadDir(cheatdir)
+				for _, f := range files {
+					fmt.Println(f.Name())
+				}
+			},
+		},
+		{
+			Name:  "edit",
+			Usage: "Add/Edit a cheat",
+			Action: func(c *cli.Context) {
+				var cheatfile = path.Join(cheatdir, c.Args().First())
+				editCheat(cheatfile, config.Editor)
+			},
+		},
+	}
 
-func mainCmd(c *cli.Context) {
-    // Read settings
-    config := &JSONData{}
-    config.ReadConfig()
-
-    if !c.Args().Present() {
-        cli.ShowAppHelp(c)
-    } else {
-        var cmdname = c.Args().First()
-        var cheatfile = path.Join(cheatdir, cmdname)
-
-        if c.Bool("edit") {
-            editCheat(cheatfile, config.Editor)
-        } else {
-            if _, err := os.Stat(cheatfile); os.IsNotExist(err) {
-                fmt.Fprintf(os.Stderr, "No cheatsheat found for '%s'\n", cmdname)
-                fmt.Fprintf(os.Stderr, "To create a new sheet, run: cheat -e %s\n", cmdname)
-                os.Exit(1)
-            } else {
-                showCheats(cheatfile, cmdname)
-            }
-        }
-    }
+	app.Run(os.Args)
 }
 
 func showCheats(cheatfile string, cmdname string) {
-    file, _ := os.Open(cheatfile)
-    scanner := bufio.NewScanner(file)
+	file, _ := os.Open(cheatfile)
+	scanner := bufio.NewScanner(file)
 
-    var i = 1
-    for scanner.Scan() {
-        line := strings.Trim(scanner.Text(), " ")
+	var i = 1
+	for scanner.Scan() {
+		line := strings.Trim(scanner.Text(), " ")
 
-        // Pretty print the output
-        // Todo: Will have to be tested on other platforms and terminals.
+		// Pretty print the output
+		// Todo: Will have to be tested on other platforms and terminals.
 
-        if strings.HasPrefix(line, "#") {
-            fmt.Println("\x1b[33;1m" + line + "\x1b[0m")
-        } else if strings.HasPrefix(line, cmdname) {
-            fmt.Println("\x1b[36;1m(" + strconv.Itoa(i) + ")\x1b[0m " + line)
-            i++
-        } else {
-            fmt.Println(line)
-        }
-    }
-    file.Close()
+		if strings.HasPrefix(line, "#") {
+			fmt.Println("\x1b[33;5m" + line + "\x1b[0m")
+		} else if strings.HasPrefix(line, cmdname) {
+			fmt.Println("\x1b[36;5m(" + strconv.Itoa(i) + ")\x1b[0m " + line)
+			i++
+		} else {
+			fmt.Println(line)
+		}
+	}
+	file.Close()
 }
 
 func editCheat(cheatfile string, configEditor string) {
-    editor, err := exec.LookPath(configEditor)
+	editor, err := exec.LookPath(configEditor)
 
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Editor not found: %s", editor)
-    }
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Editor not found: %s", editor)
+	}
 
-    cmd := exec.Command(editor, cheatfile)
-    cmd.Stdout = os.Stdout
-    cmd.Stdin = os.Stdin
-    cmd.Stderr = os.Stderr
-    cmd.Run()
+	cmd := exec.Command(editor, cheatfile)
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
